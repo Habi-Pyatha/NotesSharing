b6e456d2ba6ff4020d38e227db13fd12d11e76c1
HabiPyatha <unionhab@gmail.com>
2024-12-30 13:03:24 +0545

    devise user with username and phone

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  username               :string
+#  phone                  :string
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "Must be 10 digits" }
+end
