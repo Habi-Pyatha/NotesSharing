f8b089f907680316d14d82aa6a6c9c3b124f7f06
HabiPyatha <unionhab@gmail.com>
2025-01-01 17:15:10 +0545

    multiple images in notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -67,7 +67,7 @@ class NotesController < ApplicationController
 
   private
   def note_params
-    params.require(:note).permit(:title, :content).merge(user_id: current_user.id)
+    params.require(:note).permit(:title, :content, note_images: []).merge(user_id: current_user.id)
   end
 
   def set_note
